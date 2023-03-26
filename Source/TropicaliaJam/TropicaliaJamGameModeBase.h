efe6ec03905c5c7fa1a7f0634ee7202fedcbc9e9
lucastakejame <lucas@takejame.com.br>
2023-03-26 15:26:37 -0300

    First Commit

---
+++ Source/TropicaliaJam/TropicaliaJamGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TropicaliaJamGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TROPICALIAJAM_API ATropicaliaJamGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
